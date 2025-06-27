1fbdbc1f9a4c2228ac2623ab01a23d1dd0afa8ae
ahmetyalp <16453361+ahmetyalp@users.noreply.github.com>
2025-06-27 17:41:22 +0300

    cuda support

--- field/src/goldilocks_field.rs
+++ field/src/goldilocks_field.rs
@@ -10,6 +10,9 @@ use serde::{Deserialize, Serialize};
 use crate::ops::Square;
 use crate::types::{Field, Field64, PrimeField, PrimeField64, Sample};
 
+#[cfg(feature = "cuda")]
+use rustacuda::DeviceCopy;
+
 const EPSILON: u64 = (1 << 32) - 1;
 
 /// A field selected to have fast reduction.
@@ -20,7 +23,13 @@ const EPSILON: u64 = (1 << 32) - 1;
 ///   = 2**64 - 2**32 + 1
 ///   = 2**32 * (2**32 - 1) + 1
 /// ```
+#[cfg(not(feature = "cuda"))]
 #[derive(Copy, Clone, Serialize, Deserialize)]
+#[repr(transparent)]
+pub struct GoldilocksField(pub u64);
+
+#[cfg(feature = "cuda")]
+#[derive(Copy, Clone, Serialize, Deserialize, DeviceCopy)]
 #[repr(transparent)]
 pub struct GoldilocksField(pub u64);
 
