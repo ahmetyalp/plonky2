3375cfcd103edcd27b2c08e9678f89858014b3f4
ahmetyalp <16453361+ahmetyalp@users.noreply.github.com>
2025-07-01 16:35:49 +0300

    .

--- plonky2/src/hash/merkle_tree.rs
+++ plonky2/src/hash/merkle_tree.rs
@@ -272,7 +272,7 @@ impl<F: RichField, H: Hasher<F>> MerkleTree<F, H> {
     /// Create a Merkle proof from a leaf index.
     pub fn prove(&self, leaf_index: usize) -> MerkleProof<F, H> {
         println!("cap len: {}", self.cap.len());
-        println!("leaves len: {}", self.leaves.len());
+        println!("leaves len: {} / {}", self.leaves.len(), self.leaves_len);
         let cap_height = log2_strict(self.cap.len());
 
         #[cfg(not(feature = "cuda"))]
